950cd4d4a6825dc3cddd28f4719fa6671510c184
biroa <adam.biro@gmail.com>
2015-07-26 19:52:20 +0100

    init version 2.2

---
+++ 2.2/index.js
@@ -0,0 +1,37 @@
+//version 2.1
+
+function onReady() {
+
+    var clock = createClock('clock');
+    var clock2 = createClock('clock2');
+
+}
+
+function createClock(id){//function
+    var c = {};//or new Object();
+        c.updateClock = function(){//method
+            var clock, date, time;
+            date = new Date();
+            clock = document.getElementById(id);
+            time= formatDigits(date.getHours()) + ":"
+            + formatDigits(date.getMinutes()) + ":"
+            + formatDigits(date.getSeconds());
+            clock.innerHTML = time;
+        };
+
+    setInterval(c.updateClock,1000);
+    c.updateClock();
+
+    return c;
+}
+
+function formatDigits(num){
+    num = parseInt(num);
+    if(num < 10){
+        num = '0' + num;
+    }
+
+    return num;
+}
+
+window.onload = onReady;
