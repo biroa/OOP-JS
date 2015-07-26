ba2960f758c5715a35fefe9c557d75adc13d7503
biroa <adam.biro@gmail.com>
2015-07-26 15:43:23 +0100

    init clock version 2

---
+++ 2/index.js
@@ -0,0 +1,34 @@
+function onReady() {
+
+    var clock = createClock('clock');
+
+}
+
+function createClock(id){//function
+    var c = {};//or new Object();
+        c.updateClock = function(){//method
+            var clock, date, time;
+            date = new Date();
+            clock = document.getElementById(id);
+            time= formatDigits(date.getHours()) + ":"
+            + formatDigits(date.getMinutes()) + ":"
+            + formatDigits(date.getSeconds());
+            clock.innerHTML = time;
+        };
+
+    setInterval(c.updateClock,1000);
+    c.updateClock();
+
+    return c;
+}
+
+function formatDigits(num){
+    num = parseInt(num);
+    if(num < 10){
+        num = '0' + num;
+    }
+
+    return num;
+}
+
+window.onload = onReady;
