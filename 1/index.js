8e920acd777415937b047f079ea2919a78075613
biroa <adam.biro@gmail.com>
2015-07-26 15:26:13 +0100

    ticking clock version 0.1

--- 1/index.js
+++ 1/index.js
@@ -1,13 +1,30 @@
-function onReady(){
-    var clock,date;
+function onReady() {
+
+    setInterval(updateClock,1000);
+    updateClock();
+}
+
+function updateClock(){
+    var clock, date, time;
     //property variable in a function
     //method is function in a function
     date = new Date();
-    console.log(date);
     //this is the element of the DOM (Document Object Model)
     clock = document.getElementById('clock');
+    time= formatDigits(date.getHours()) + ":"
+    + formatDigits(date.getMinutes()) + ":"
+    + formatDigits(date.getSeconds());
+    console.log(time);
+    clock.innerHTML = time;
+}
 
-    clock.innerHTML = date.getHours()+":"+ date.getDate() +":"+ date.getMinutes();
+function formatDigits(num){
+    num = parseInt(num);
+    if(num < 10){
+        num = '0' + num;
+    }
+
+    return num;
 }
 
 window.onload = onReady;
