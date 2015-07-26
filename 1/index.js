95545dcec8d513eca3c764b278612bdfd1c3949b
biroa <adam.biro@gmail.com>
2015-07-26 14:52:40 +0100

    write out current time

--- 1/index.js
+++ 1/index.js
@@ -1,5 +1,13 @@
 function onReady(){
-    console.log('loaded');
+    var clock,date;
+    //property variable in a function
+    //method is function in a function
+    date = new Date();
+    console.log(date);
+    //this is the element of the DOM (Document Object Model)
+    clock = document.getElementById('clock');
+
+    clock.innerHTML = date.getHours()+":"+ date.getDate() +":"+ date.getMinutes();
 }
 
-window.onload = onReady();
+window.onload = onReady;
