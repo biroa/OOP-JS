4f7f6d8284e3515354a955521c298c402caa5803
biroa <adam.biro@gmail.com>
2015-07-29 21:58:46 +0100

    version 3.4 namespace usage for domain packaging

---
+++ 3.4/index.js
@@ -0,0 +1,116 @@
+//version 3.4
+//namespace usage for domain package
+
+function onReady() {
+
+    var clock = new eu.adambiro.Clock('clock');
+    var clock2 = new eu.adambiro.Clock('clock2', -7200000, 'ETC');
+    var d = new Date();
+}
+
+//Static properties
+Date.__interval = 0;
+Date.__aDates = [];
+/**
+ * Static method
+ * @param date
+ */
+Date.addToInterval = function (date) {
+    this.__aDates.push(date);
+
+    if (!Date.__interval) {
+        Date.__interval = setInterval(function () {
+            Date.updateDates()
+        }, 1000)
+    }
+}
+
+Date.updateDates = function () {
+    for (var i = 0; i < this.__aDates.length; i++) {
+        this.__aDates[i].updateSeconds();
+    }
+}
+/**
+ * Updating seconds
+ */
+Date.prototype.updateSeconds = function () {
+    this.setSeconds(this.getSeconds() + 1);
+}
+
+
+/**
+ * Auto update
+ * @param isAuto
+ */
+Date.prototype.autoClock = function (isAuto) {
+    clearInterval(this.clockInterval);
+    if (isAuto) {
+        //var that = this;
+        //this.clockInterval = setInterval(function () {
+        //    that.updateSeconds();
+        //}, 1000);
+        Date.addToInterval(this);
+    }
+}
+
+
+// namespace usage in js
+// basic obj. = name of the actual site adambiro.eu
+// eu.adambiro.Projectname
+
+var eu = eu || {}; //do not override the eu folder if it exists
+eu.adambiro = eu.adambiro || {};
+
+
+/**
+ * @param id
+ * @param offset
+ * @param label
+ * @constructor
+ */
+eu.adambiro.Clock = function (id, offset, label) {
+    offset = offset || 0;
+    label = label || 'UTC';
+    var d = new Date();
+    var offset = (offset + d.getTimezoneOffset()) * 60 * 1000;
+    //turn minutes to sec(*60) and millisecond(*1000)
+    this.d = new Date(offset + d.getTime());
+    this.d.autoClock(true);
+    this.id = id;
+    this.label = label;
+
+    var that = this;
+    setInterval(function () {
+        that.updateClock()
+    }, 1000);
+    this.updateClock();
+}
+/**
+ * We deleted the multiple Date object instantiation
+ */
+eu.adambiro.Clock.prototype.updateClock = function () {
+    var clock, date, time;
+    date = this.d;
+    //date.updateSeconds();
+    clock = document.getElementById(this.id);
+    time = this.formatDigits(date.getHours()) + ":"
+    + this.formatDigits(date.getMinutes()) + ":"
+    + this.formatDigits(date.getSeconds()) + " " + this.label;
+    clock.innerHTML = time;
+};
+/**
+ * Convert digits to 0X format if they are smaller than 10
+ *
+ * @param num
+ * @returns {Number}
+ */
+eu.adambiro.Clock.prototype.formatDigits = function (num) {
+    num = parseInt(num);
+    if (num < 10) {
+        num = '0' + num;
+    }
+
+    return num;
+}
+
+window.onload = onReady;
